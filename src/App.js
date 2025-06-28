c431d79b6ecf65059152db4ae3ff6418e5e1d0f9
Mscode04 <ms.shaheenkp@gmail.com>
2025-06-29 00:50:22 +0530

    updated1.0

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Checkin from "./Pages/Checkin";
 import GasRequests from "./Pages/GasRequests";
 import UpdateSale from "./Components/UpdateSale";
 import AllMessages from "./Pages/AllMessages";
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(
     localStorage.getItem("isAuthenticated") === "true"
@@ -30,36 +31,34 @@ function App() {
   };
 
   return (
-    
-      <Routes>
-        <Route path="/login" element={!isAuthenticated ? <LoginPage setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/dashboard" />} />
-        <Route path="logout" element={<Logout onLogout={handleLogout} />} />
-        
-        {/* Protected routes */}
-        <Route path="/" element={<Main />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="new-connection" element={<NewConnection />} />
-          <Route path="sales" element={<Sales />} />
-          <Route path="routes" element={<Routess/>} />
-          <Route path="products" element={<Products />} />
-          <Route path="help" element={<Help />} />
-          <Route path="check-in" element={<Checkin />} />
-          <Route path="req" element={<GasRequests/>} />
-          <Route path="msg" element={<AllMessages/>} />
+    <Routes>
+      <Route path="/" element={!isAuthenticated ? <Navigate to="/login" /> : <Navigate to="/dashboard" />} />
+      <Route path="/login" element={!isAuthenticated ? <LoginPage setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/dashboard" />} />
+      <Route path="/logout" element={<Logout onLogout={handleLogout} />} />
+      
+      {/* Protected routes */}
+      <Route path="/" element={isAuthenticated ? <Main /> : <Navigate to="/login" />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="new-connection" element={<NewConnection />} />
+        <Route path="sales" element={<Sales />} />
+        <Route path="routes" element={<Routess/>} />
+        <Route path="products" element={<Products />} />
+        <Route path="help" element={<Help />} />
+        <Route path="check-in" element={<Checkin />} />
+        <Route path="req" element={<GasRequests/>} />
+        <Route path="msg" element={<AllMessages/>} />
 
-          <Route path="all-customers" element={<AllCustomers />} />
-            <Route path="all-sales" element={<AllSales />} />
-<Route path="/customer/:id" element={<CustomerProfile />} />
-<Route path="/customer-edit/:id" element={<EditCustomer />} />
-<Route path="/sales/:id" element={<SaleDetail />} />
-<Route path="/sales/update/:id" element={<UpdateSale />} />
-          
-        </Route>
+        <Route path="all-customers" element={<AllCustomers />} />
+        <Route path="all-sales" element={<AllSales />} />
+        <Route path="customer/:id" element={<CustomerProfile />} />
+        <Route path="customer-edit/:id" element={<EditCustomer />} />
+        <Route path="sales/:id" element={<SaleDetail />} />
+        <Route path="sales/update/:id" element={<UpdateSale />} />
+      </Route>
 
-        {/* Redirect to dashboard if authenticated but invalid route */}
-        <Route path="*" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
-      </Routes>
-   
+      {/* Redirect to login if not authenticated */}
+      <Route path="*" element={!isAuthenticated ? <Navigate to="/login" /> : <Navigate to="/dashboard" />} />
+    </Routes>
   );
 }
 
